f34d102771059561cb78b200005f48bcdb41fd87
Matt and Terra’s Adventures <Mattjhagen@users.noreply.github.com>
2025-06-20 15:22:30 -0500

    Update validators.js

--- validators.js
+++ validators.js
@@ -7,19 +7,19 @@ const resumeSchema = Joi.object({
 
 const domainSchema = Joi.object({
   email: Joi.string().email().required(),
-  type: Joi.string().min(1).required(),
-  domain: Joi.string().uri({ allowRelative: false }).required()
+  type: Joi.string().required(),
+  domain: Joi.string().uri().required()
 });
 
 function validateResume(req, res, next) {
   const { error } = resumeSchema.validate(req.body);
-  if (error) return res.status(400).send(`Validation error: ${error.message}`);
+  if (error) return res.status(400).send(error.message);
   next();
 }
 
 function validateDomain(req, res, next) {
   const { error } = domainSchema.validate(req.body);
-  if (error) return res.status(400).send(`Validation error: ${error.message}`);
+  if (error) return res.status(400).send(error.message);
   next();
 }
 

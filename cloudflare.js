6b1fed740c4d1cadb2b2063b21d9b31f09d5e512
Matt and Terra’s Adventures <Mattjhagen@users.noreply.github.com>
2025-06-20 15:22:02 -0500

    Update cloudflare.js

--- cloudflare.js
+++ cloudflare.js
@@ -5,7 +5,7 @@ const CLOUDFLARE_API = 'https://api.cloudflare.com/client/v4';
 
 const ZONE_ID = process.env.CLOUDFLARE_ZONE_ID;
 const API_TOKEN = process.env.CLOUDFLARE_API_TOKEN;
-const ROOT_DOMAIN = 'pacmacmobile.com';
+const ROOT_DOMAIN = process.env.ROOT_DOMAIN;
 
 async function createSubdomainRecord(subdomain) {
   const fullDomain = `${subdomain}.${ROOT_DOMAIN}`;
